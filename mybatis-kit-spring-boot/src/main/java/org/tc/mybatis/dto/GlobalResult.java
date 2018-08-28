632c707ff7ca26f97aab94f0e7a2d83bc64364ef
since2012 <to.be.an.engineer@users.noreply.github.com>
2018-08-28 16:54:18 +0800

    init

---
+++ mybatis-kit-spring-boot/src/main/java/org/tc/mybatis/dto/GlobalResult.java
@@ -0,0 +1,174 @@
+package org.tc.mybatis.dto;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 自定义响应数据结构
+ * 这个类是提供给门户，ios，安卓，微信商城用的
+ * 门户接受此类数据后需要使用本类的方法转换成对于的数据类型格式（类，或者list）
+ * 其他自行处理
+ * 200：表示成功
+ * 500：表示错误，错误信息在msg字段中
+ * 501：bean验证错误，不管多少个错误都以map形式返回
+ * 502：拦截器拦截到用户token出错
+ * 555：异常抛出信息
+ */
+@Data
+public class GlobalResult<T> {
+
+    // 定义jackson对象
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    // 响应业务状态
+    private Integer code;
+    // 响应消息
+    private String msg;
+    // 响应中的数据
+    private T data;
+
+    /**
+     * 成功
+     *
+     * @param data
+     */
+    public GlobalResult(T data) {
+        this.code = 200;
+        this.msg = "操作成功";
+        this.data = data;
+    }
+
+    //失败
+    public GlobalResult(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    //失败
+    public GlobalResult(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    //成功带数据
+    public static <T> GlobalResult ok(T data) {
+        return new GlobalResult(data);
+    }
+
+    //成功但无数据
+    public static GlobalResult ok() {
+        return new GlobalResult(null);
+    }
+
+    //失败信息
+    public static GlobalResult errorMsg(String msg) {
+        return new GlobalResult(500, msg);
+    }
+
+    //根据布尔值返回数据
+    public static GlobalResult tip(boolean result) {
+        if (result) {
+            return GlobalResult.ok();
+        }
+        return GlobalResult.errorMsg("操作失败");
+    }
+
+
+    public static <T> GlobalResult errorMap(T data) {
+        return new GlobalResult(501, "error", data);
+    }
+
+    public static GlobalResult errorTokenMsg(String msg) {
+        return new GlobalResult(502, msg);
+    }
+
+    public static GlobalResult errorException(String msg) {
+        return new GlobalResult(555, msg);
+    }
+
+    public static GlobalResult build(Integer code, String msg, Object data) {
+        return new GlobalResult(code, msg, data);
+    }
+
+    public static GlobalResult build(Integer code, String msg) {
+        return new GlobalResult(code, msg, null);
+    }
+
+    public Boolean isOK() {
+        return this.code == 200;
+    }
+
+    /**
+     * @param jsonData
+     * @param clazz
+     * @return
+     * @Description: 将json结果集转化为LeeJSONResult对象
+     * 需要转换的对象是一个类
+     * @author leechenxiang
+     * @date 2016年4月22日 下午8:34:58
+     */
+    public static GlobalResult formatToPojo(String jsonData, Class<?> clazz) {
+        try {
+            if (clazz == null) {
+                return MAPPER.readValue(jsonData, GlobalResult.class);
+            }
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (clazz != null) {
+                if (data.isObject()) {
+                    obj = MAPPER.readValue(data.traverse(), clazz);
+                } else if (data.isTextual()) {
+                    obj = MAPPER.readValue(data.asText(), clazz);
+                }
+            }
+            return build(jsonNode.get("code").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param json
+     * @return
+     * @Description: 没有object对象的转化
+     * @author leechenxiang
+     * @date 2016年4月22日 下午8:35:21
+     */
+    public static GlobalResult format(String json) {
+        try {
+            return MAPPER.readValue(json, GlobalResult.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @param jsonData
+     * @param clazz
+     * @return
+     * @Description: Object是集合转化
+     * 需要转换的对象是一个list
+     * @author leechenxiang
+     * @date 2016年4月22日 下午8:35:31
+     */
+    public static GlobalResult formatToList(String jsonData, Class<?> clazz) {
+        try {
+            JsonNode jsonNode = MAPPER.readTree(jsonData);
+            JsonNode data = jsonNode.get("data");
+            Object obj = null;
+            if (data.isArray() && data.size() > 0) {
+                obj = MAPPER.readValue(data.traverse(),
+                        MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+            }
+            return build(jsonNode.get("code").intValue(), jsonNode.get("msg").asText(), obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
---
+++ mybatis-kit-spring-boot/src/main/java/org/tc/mybatis/service/impl/BaseServiceImpl.java
@@ -0,0 +1,185 @@
+/**
+ * Copyright (c) 2011-2016, hubin (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.tc.mybatis.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.tc.mybatis.dao.MyMapper;
+import org.tc.mybatis.service.IBaseService;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+/**
+ * <p>
+ * IBaseService 实现类（ 泛型：M 是 mapper 对象，T 是实体 ， PK 是主键泛型 ）
+ * </p>
+ *
+ * @author hubin
+ * @Date 2016-04-20
+ */
+public class BaseServiceImpl<M extends MyMapper<T>, T> implements IBaseService<T> {
+
+    @Autowired
+    protected M baseMapper;
+
+    /**
+     * 判断数据库操作是否成功
+     * 注意！！ 该方法为 Integer 判断，不可传入 int 基本类型
+     *
+     * @param result 数据库操作返回影响条数
+     * @return boolean
+     */
+    protected static boolean retBool(int result) {
+        return result >= 1;
+    }
+
+    @Override
+    public boolean existsByPK(Object pk) {
+        return baseMapper.existsWithPrimaryKey(pk);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean insertUseGeneratedKeys(T entity) {
+        return retBool(baseMapper.insertUseGeneratedKeys(entity));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean insertSelective(T entity) {
+        return retBool(baseMapper.insertSelective(entity));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean insertAllCol(T entity) {
+        return retBool(baseMapper.insert(entity));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean insertList(List<T> entityList) {
+        if (CollectionUtils.isEmpty(entityList)) {
+            throw new IllegalArgumentException("Error: entityList must not be empty");
+        }
+        return retBool(baseMapper.insertList(entityList));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean insertOrUpdateSelective(T entity, Object pk) {
+        int result = 0;
+        if (pk == null) {
+            result = baseMapper.insertSelective(entity);
+        } else {
+            result = baseMapper.updateByPrimaryKeySelective(entity);
+        }
+        return retBool(result);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean insertOrUpdateAllCol(T entity, Object pk) {
+        int result = 0;
+        if (pk == null) {
+            result = baseMapper.insert(entity);
+        } else {
+            result = baseMapper.updateByPrimaryKey(entity);
+        }
+        return retBool(result);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean deleteByPK(Object pk) {
+        return retBool(baseMapper.deleteByPrimaryKey(pk));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean deleteByEqualTo(Class<?> entityClass, String field, Object value) {
+        Example example = new Example(entityClass);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo(field, value);
+        int count = baseMapper.deleteByExample(example);
+        return retBool(count);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean deleteByIn(Class<?> entityClass, String field, List<Object> value) {
+        Example example = new Example(entityClass);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andIn(field, value);
+        int count = baseMapper.deleteByExample(example);
+        return retBool(count);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean updateByPKSelective(T entity) {
+        return retBool(baseMapper.updateByPrimaryKeySelective(entity));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public boolean updateAllColByPK(T entity) {
+        return retBool(baseMapper.updateByPrimaryKey(entity));
+    }
+
+    @Override
+    public T selectByPK(Object pk) {
+        return baseMapper.selectByPrimaryKey(pk);
+    }
+
+    @Override
+    public List<T> selectAll() {
+        return baseMapper.selectAll();
+    }
+
+    @Override
+    public List<T> selectByEqual(Class<?> entityClass, String field, Object value) {
+        Example example = new Example(entityClass);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo(field, value);
+        List<T> list = baseMapper.selectByExample(example);
+        return list;
+    }
+
+    @Override
+    public List<T> selectByIn(Class<?> entityClass, String field, List<Object> value) {
+        Example example = new Example(entityClass);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andIn(field, value);
+        List<T> list = baseMapper.selectByExample(example);
+        return list;
+    }
+
+    @Override
+    public PageInfo<T> selectPage(Example example, int pageNum, int pageSize) {
+        // 开始分页
+        PageHelper.startPage(pageNum, pageSize);
+        List<T> list = baseMapper.selectByExample(example);
+        //用PageInfo对结果进行包装
+        PageInfo<T> page = new PageInfo<T>(list);
+        return page;
+    }
+
+}
